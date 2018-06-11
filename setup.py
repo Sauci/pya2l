6e3d05540459e09a5c01b332309a819177c1e64a
Sauci <guillaumesottas@gmail.com>
2018-06-11 15:08:44 -0600

    add missing required package pytest.

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     author='Guillaume Sottas',
     author_email='guillaumesottas@gmail.com',
     description='utility for a2l files',
-    install_requires='ply',
+    install_requires=['ply',
+                      'pytest'],
     dependency_links=[
         'https://pypi.python.org/packages/e5/69/882ee5c9d017149285cab114ebeab373308ef0f874fcdac9beb90e0ac4da/ply-3.11.tar.gz#md5=6465f602e656455affcd7c5734c638f8'
     ]
